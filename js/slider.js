3c4196ccb7e3fb7c2880c39f7af68355d8027ca6
Tomasz Lewandowski <tomek2x1@wp.pl>
2021-04-20 11:50:48 +0200

    first commit

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+      $('.productSlider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows:true,
+        prevArrow:'<button type="button" data-role="none" class="slick-prev">Previous</button>',
+        nextArrow:'<button type="button" data-role="none" class="slick-next">Next</button>',
+        autoplay:true,
+      });
+    });
+    
